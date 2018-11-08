c4e0af8118e59e879c0756c912c340b6ab48bfe3
Ilias Ismanalijev <me@il.ly>
2018-11-08 14:41:23 +0100

    br

--- testpages/Modal/Modal.vue
+++ testpages/Modal/Modal.vue
@@ -5,7 +5,9 @@
 		<Modal :show="show" @close="show = false" width="400">
 				<div style="padding: 20px;">
 				<h1>Hello, I'm a Modal!</h1>
-				<p>By default I freeze your background.</br> Click and scroll around to test my behaviour.</br> If you have ideas to prettify me, please tell Giupetto..</p>
+				<p>By default I freeze your background.<br> 
+				Click and scroll around to test my behaviour.<br> 
+				If you have ideas to prettify me, please tell Giupetto..</p>
 				</div>
 		</Modal>
 	</div>
