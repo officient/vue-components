eed23943ef4747680da2ed2034770ff5fcd1ae76
SemProvoost <semprovoost@Sems-MacBook-Pro.local>
2018-11-13 10:53:13 +0100

    remove tooltip in calendardaysmobile

--- src/components/daysoff/CalendarDaysMobile.vue
+++ src/components/daysoff/CalendarDaysMobile.vue
@@ -2,15 +2,7 @@
   <div>
     <!-- eslint-disable-next-line vue/no-unused-vars -->
     <div class="calendar-day" v-for="i in blankDays">&nbsp;</div>
-
     <div class="calendar-day" v-for="d in days" :key="d.i" :class="d.classBools" @click="toggleDay(d)">
-      <tooltip class="calendar-tooltip">
-        <calendar-tooltip-content
-          :weekly-schedule="weekly_schedule"
-          :dateStr="d.dateStr"
-          :slots="d.items"
-        />
-      </tooltip>
       <div class="content">
         <div class="rows">
           <div v-for="item in d.items" class="row" :style="item.barStyles"></div>
@@ -22,17 +14,9 @@
 </template>
 
 <script>
-import '../../scss/Tooltip.scss'
-import Tooltip from '../Tooltip'
-import CalendarTooltipContent from './CalendarTooltipContent'
-
 export default {
   name: 'calendar-days-mobile',
-
   props: [ 'blankDays', 'days', 'weekly_schedule' ],
-
-  components: { Tooltip, CalendarTooltipContent },
-
   methods: {
     toggleDay (d) {
       this.$emit('onToggleDay', d)
