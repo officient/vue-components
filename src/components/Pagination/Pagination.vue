b55aee8db8af00da21b6217c8e6f3a0631d41367
Thomas Vrf <thomas.vrf@gmail.com>
2018-11-13 14:55:38 +0100

    pagination with totalcount, add all usecase tests

--- src/components/Pagination/Pagination.vue
+++ src/components/Pagination/Pagination.vue
@@ -3,7 +3,7 @@
     <div class="arrow left" @click="prevPage" :class="{ disabled: isPrevDisabled }">
       <i class="mdi mdi-angle-left"></i>
     </div>
-    <div class="text">{{ humanizedPage }}</div>
+    <div class="text">{{ pageText }}</div>
     <div class="arrow right" @click="nextPage" :class="{ disabled: isNextDisabled }">
       <i class="mdi mdi-angle-right"></i>
     </div>
@@ -27,17 +27,50 @@ export default {
     pageCount: {
       type: Number,
       required: true
+    },
+    totalCount: {
+      type: Number,
+      default: 0
     }
   },
   computed: {
+    pageText () {
+      if (this.totalCount) {
+        // if we have total count, compute totalPageCount
+        // and append to string: <n/N>
+        return `${this.humanizedPage} / ${this.totalPageCount}`
+      } else {
+        // no total count just show page number: <n>
+        return `${this.humanizedPage}`
+      }
+    },
     humanizedPage () {
+      // api is zerobased
       return this.page + 1
     },
+    totalPageCount () {
+      // only computed when totalCount is set
+      return Math.ceil(this.totalCount / this.perPage) || 0
+    },
     isPrevDisabled () {
+      // page is 0, can not go back further
       return this.page === 0
     },
     isNextDisabled () {
-      return this.pageCount < this.perPage
+      // end of pagination
+      // page contains less items than max allowed
+      if (this.pageCount < this.perPage) {
+        return true
+      }
+
+      // end of pagination
+      // if we have total count, compute totalPageCount
+      // and check if current page is >= than total pages
+      if (this.totalCount && this.humanizedPage >= this.totalPageCount) {
+        return true
+      }
+
+      return false
     }
   },
   methods: {
--- tests/Pagination.test.js
+++ tests/Pagination.test.js
@@ -15,6 +15,32 @@ describe('Pagination.vue', () => {
       expect(wrapper.vm.humanizedPage).toBe(1)
     })
 
+    it('pageText is humanizedPage: 1', () => {
+      const wrapper = shallow(Pagination, {
+        propsData: {
+          page: 0,
+          perPage: 10,
+          pageCount: 9
+        }
+      })
+
+      expect(wrapper.vm.humanizedPage).toBe(1)
+      expect(wrapper.vm.pageText).toBe('1')
+    })
+
+    it('pageText is humanizedPage: 2', () => {
+      const wrapper = shallow(Pagination, {
+        propsData: {
+          page: 1,
+          perPage: 10,
+          pageCount: 9
+        }
+      })
+
+      expect(wrapper.vm.humanizedPage).toBe(2)
+      expect(wrapper.vm.pageText).toBe('2')
+    })
+
     it('isPrevDisabled returns true when on first page (0)', () => {
       const wrapper = shallow(Pagination, {
         propsData: {
@@ -89,6 +115,219 @@ describe('Pagination.vue', () => {
 
       wrapper.vm.nextPage()
       expect(wrapper.emitted('onPageChanged')).toEqual([[ 2 ]])
+    })
+  })
+
+  describe('totalCount', () => {
+    // totalPageCount
+    it('computed totalPageCount is rounded up total / per page', () => {
+      const wrapper = shallow(Pagination, {
+        propsData: {
+          page: 1,
+          perPage: 10,
+          pageCount: 10,
+          totalCount: 21
+        }
+      })
+      expect(wrapper.vm.totalPageCount).toBe(3)
+
+      wrapper.setProps({ totalCount: 20 })
+      expect(wrapper.vm.totalPageCount).toBe(2)
+
+      wrapper.setProps({ totalCount: 19 })
+      expect(wrapper.vm.totalPageCount).toBe(2)
+    })
+
+    it('if pagecount is less than max allowed items, disable next', () => {
+      // 9 items out of 10 items per page, next is disabled
+      const wrapper = shallow(Pagination, {
+        propsData: {
+          page: 0,
+          perPage: 10,
+          pageCount: 9,
+          totalCount: 19
+        }
+      })
+
+      expect(wrapper.vm.isNextDisabled).toBe(true)
+    })
+
+    it('if page is higher to max pages, disable next', () => {
+      const wrapper = shallow(Pagination, {
+        propsData: {
+          page: 2,
+          perPage: 10,
+          pageCount: 9,
+          totalCount: 19
+        }
+      })
+
+      expect(wrapper.vm.humanizedPage).toBe(3)
+      expect(wrapper.vm.totalPageCount).toBe(2)
+      expect(wrapper.vm.isNextDisabled).toBe(true)
+    })
+
+    it('if page is equal to max pages, disable next', () => {
+      const wrapper = shallow(Pagination, {
+        propsData: {
+          page: 1,
+          perPage: 10,
+          pageCount: 9,
+          totalCount: 19
+        }
+      })
+
+      expect(wrapper.vm.humanizedPage).toBe(2)
+      expect(wrapper.vm.totalPageCount).toBe(2)
+      expect(wrapper.vm.isNextDisabled).toBe(true)
+    })
+
+    it('if page is lower than max pages and page count is perpage count, enable next', () => {
+      const wrapper = shallow(Pagination, {
+        propsData: {
+          page: 1,
+          perPage: 10,
+          pageCount: 10,
+          totalCount: 21
+        }
+      })
+
+      expect(wrapper.vm.humanizedPage).toBe(2)
+      expect(wrapper.vm.totalPageCount).toBe(3)
+      expect(wrapper.vm.isNextDisabled).toBe(false)
+    })
+
+    it('text adds "/ maxpages" with totalCount', () => {
+      const wrapper = shallow(Pagination, {
+        propsData: {
+          page: 1,
+          perPage: 10,
+          pageCount: 10,
+          totalCount: 21
+        }
+      })
+
+      expect(wrapper.vm.humanizedPage).toBe(2)
+      expect(wrapper.vm.totalPageCount).toBe(3)
+      expect(wrapper.vm.pageText).toBe('2 / 3')
+    })
+  })
+
+  describe('all use cases with disabled and text', () => {
+    it('next disabled prev disabled', () => {
+      const wrapper = shallow(Pagination, {
+        propsData: {
+          page: 0,
+          perPage: 10,
+          pageCount: 9,
+        }
+      })
+
+      expect(wrapper.vm.isNextDisabled).toBe(true)
+      expect(wrapper.vm.isPrevDisabled).toBe(true)
+      expect(wrapper.vm.pageText).toBe('1')
+    })
+
+    it('next enabled prev disabled', () => {
+      const wrapper = shallow(Pagination, {
+        propsData: {
+          page: 0,
+          perPage: 10,
+          pageCount: 10,
+        }
+      })
+
+      expect(wrapper.vm.isNextDisabled).toBe(false)
+      expect(wrapper.vm.isPrevDisabled).toBe(true)
+      expect(wrapper.vm.pageText).toBe('1')
+    })
+
+    it('next disabled prev enabled', () => {
+      const wrapper = shallow(Pagination, {
+        propsData: {
+          page: 1,
+          perPage: 10,
+          pageCount: 9,
+        }
+      })
+
+      expect(wrapper.vm.isNextDisabled).toBe(true)
+      expect(wrapper.vm.isPrevDisabled).toBe(false)
+      expect(wrapper.vm.pageText).toBe('2')
+    })
+
+    it('next enabled prev enabled', () => {
+      const wrapper = shallow(Pagination, {
+        propsData: {
+          page: 1,
+          perPage: 10,
+          pageCount: 10,
+        }
+      })
+
+      expect(wrapper.vm.isNextDisabled).toBe(false)
+      expect(wrapper.vm.isPrevDisabled).toBe(false)
+      expect(wrapper.vm.pageText).toBe('2')
+    })
+
+    it('next disabled prev disabled, with totalCount', () => {
+      const wrapper = shallow(Pagination, {
+        propsData: {
+          page: 0,
+          perPage: 10,
+          pageCount: 10,
+          totalCount: 10
+        }
+      })
+
+      expect(wrapper.vm.isNextDisabled).toBe(true)
+      expect(wrapper.vm.isPrevDisabled).toBe(true)
+      expect(wrapper.vm.pageText).toBe('1 / 1')
+    })
+
+    it('next enabled prev disabled, with totalCount', () => {
+      const wrapper = shallow(Pagination, {
+        propsData: {
+          page: 0,
+          perPage: 10,
+          pageCount: 10,
+          totalCount: 19
+        }
+      })
+
+      expect(wrapper.vm.isNextDisabled).toBe(false)
+      expect(wrapper.vm.isPrevDisabled).toBe(true)
+      expect(wrapper.vm.pageText).toBe('1 / 2')
+    })
+
+    it('next disabled prev enabled, with totalCount', () => {
+      const wrapper = shallow(Pagination, {
+        propsData: {
+          page: 1,
+          perPage: 10,
+          pageCount: 10,
+          totalCount: 20
+        }
+      })
+
+      expect(wrapper.vm.isNextDisabled).toBe(true)
+      expect(wrapper.vm.isPrevDisabled).toBe(false)
+      expect(wrapper.vm.pageText).toBe('2 / 2')
+    })
+
+    it('next enabled prev enabled, with totalCount', () => {
+      const wrapper = shallow(Pagination, {
+        propsData: {
+          page: 1,
+          perPage: 10,
+          pageCount: 10,
+          totalCount: 21
+        }
+      })
+
+      expect(wrapper.vm.isNextDisabled).toBe(false)
+      expect(wrapper.vm.isPrevDisabled).toBe(false)
+      expect(wrapper.vm.pageText).toBe('2 / 3')
     })
   })
 })
