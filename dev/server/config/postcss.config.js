d00ce7722c0a72f6e2c0a3d3660d24475ca67635
Dave Irvine <dave@daveirvine.me>
2020-05-19 15:30:51 +0100

    Remove dev, tests, jest and babel configs

--- dev/server/config/postcss.config.js
+++
@@ -1,9 +0,0 @@
-const autoprefixer = require('autoprefixer')
-
-module.exports = {
-  plugins: [
-    autoprefixer({
-      browsers: ["> 1%", "Last 2 versions", "ie >= 9", "safari >= 7"]
-    })
-  ]
-}
--- tests/stubJestCSS.js
+++
@@ -1,3 +0,0 @@
-// Stub CSS for Jest tests
-// because css is loaded from vue-components
-module.exports = {}
